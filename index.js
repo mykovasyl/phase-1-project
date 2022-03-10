f566f488f44c1f647a9b5f9476b2458a12908ec5
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-03-09 20:23:27 -0500

    update for Nison

--- index.js
+++ index.js
@@ -2,20 +2,46 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const classDropdown = document.querySelector('#fighter-classes')
   const fighterClass = document.querySelector('#fighter-class')
-  const nameDescription = document.querySelector('#name-and-description')
-  const nameInput = document.querySelector('#name-fighter')
-  const descInput = document.querySelector('#describe-fighter')
-  
+  const nameDescrForm = document.querySelector('#name-descr-form')
+  const fighterName = document.querySelector('#fighter-name')
+  const fighterPower = document.querySelector('#power-level')
+  const fighterDescription = document.querySelector('#fighter-description')
+  const powerLevelBtn = document.querySelector('#power')
+  const fighterPowerLevel = document.querySelector('#fighter-power-level')
+  const enterArena = document.querySelector('#enter-arena')
+  const setUpFighter = document.querySelector('#set-up-fighter')
+  const readyForBattle = document.querySelector('#ready-for-battle')
+
+
   //add fighter class, unhide name/description inputs
   classDropdown.addEventListener('change', (e) => {
     fighterClass.textContent = e.target.value
-    nameDescription.removeAttribute('hidden')
+    nameDescrForm.removeAttribute('hidden')
   })
 
-  document.addEventListener('click', (e) => {
-    console.log(e)
+  //add name and description, unhide power level buttons
+  nameDescrForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    fighterName.textContent = e.target[0].value
+    fighterDescription.textContent = e.target[1].value
+    fighterPower.removeAttribute('hidden')
   })
 
+  powerLevelBtn.addEventListener('click', (e) => {
+    fighterPowerLevel.textContent = powerLevelBtn.textContent = getRandomNumber(1, 100)
+    powerLevelBtn.disabled = true
+    readyForBattle.removeAttribute('hidden')
+  })
+
+  function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  
+  enterArena.addEventListener('click', (e) => {
+    setUpFighter.hidden = true
+  })
 
 
 
